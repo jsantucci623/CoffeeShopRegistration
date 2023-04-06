507bd302a581e6e88df3a4ba39b1cd76bcdc53c2
Jeff Santucci <jeffsantucci@rocketmortgage.com>
2023-04-06 19:54:54 -0400

    Coffee Shop Registration Lab - Jeff Santucci

---
+++ CoffeeShopRegistrationApp/CoffeeShopRegistration/Controllers/RegistrationController.cs
@@ -0,0 +1,54 @@
+﻿using CoffeeShopRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopRegistration.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        /* private readonly ILogger<HomeController> _logger;
+
+         public HomeController(ILogger<HomeController> logger)
+         {
+             _logger = logger;
+         }
+        */
+        [HttpPost]
+        public IActionResult HandleSubmit(UserProfile userProfile)
+        {
+            if (!ModelState.IsValid)    // very handy tool
+            {
+                return RedirectToAction("Index");
+            }
+            // We can process the UserProfile object now!
+            // For example, we can save it to a database.
+
+            return RedirectToAction("Welcome", userProfile);
+        }
+
+        public IActionResult Register(UserProfile userProfile) 
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(UserProfile userProfile)
+        {
+            return View(userProfile);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoffeeShopRegistrationApp/CoffeeShopRegistration/Models/UserProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace CoffeeShopRegistration.Models
+{
+    public class UserProfile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+
+    }
+}
